b4438be06f67415152fc2bbf0e79e5505e8619e1
ViVi O <1446032753@qq.com>
2017-02-08 20:19:45 +0800

    完成了对文件夹下建立索引的功能

---
+++ main/engine.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"strconv"
+	"time"
+	"bufio"
+)
+
+const (
+	path = "/Users/CodeMania/Documents/编程相关"
+	indexfile = "/Users/CodeMania/.vpm/index.dat"
+)
+
+var files []string
+
+func listFunc(path string, f os.FileInfo, err error) error {
+	if f == nil {
+		return err
+	}
+
+	if f.IsDir() {
+		return nil
+	}
+
+	nameParts := strings.Split(f.Name(), ".")
+	fileName := strings.Replace(nameParts[0], " ", "_", -1)
+	suffix := "nil"
+	if len(nameParts) > 1 {
+		suffix = nameParts[len(nameParts) - 1]
+	}
+	size := strconv.FormatInt(f.Size(), 10)
+	path = "." + strings.Replace(path, " ", "_", -1)[39:]
+	modtime := f.ModTime().Format(time.RFC3339)
+
+	fInfo := fileName + " " + suffix + " " + size + " " + path + " " + modtime + "\n"
+	files = append(files, fInfo)
+	return nil
+}
+
+func FileListUpdate(path string) {
+	err := filepath.Walk(path, listFunc)
+	if err != nil {
+		fmt.Printf("filepath.Walk() returned %v\n", err)
+	}
+
+	os.Remove(indexfile)
+
+	outputFile, outputError := os.OpenFile(indexfile, os.O_WRONLY|os.O_CREATE, 0666)
+	if outputError != nil {
+		fmt.Printf("An error occurred with file opening or creation\n")
+		return
+	}
+	defer outputFile.Close()
+
+	outputWriter := bufio.NewWriter(outputFile)
+
+	for i := 0; i < len(files); i++ {
+		num := strconv.Itoa(i + 1)
+		num = num + strings.Repeat(" ", 8 - len(num))
+		outputWriter.WriteString(num + files[i])
+	}
+	outputWriter.Flush()
+}
+
+func main() {
+	FileListUpdate(path)
+}
